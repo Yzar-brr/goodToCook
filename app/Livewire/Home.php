665c751350ca8a36484e18801ba643ec683fdcd6
Sofiane <55191416+Yzar-brr@users.noreply.github.com>
2025-04-24 20:07:25 +0200

    Update Home.php

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -25,7 +25,7 @@ class Home extends Component
         if($this->researchRecipe != ''){
             $this->recipes = Recipe::where('name', 'like', '%'.$this->researchRecipe.'%')->get()->whereIn('confirmed', 1);
         }else{
-            $this->recipes = Recipe::all()->whereIn('confirmed', 1);
+            $this->recipes = Recipe::all()->whereIn('confirmed', 1)->sortByDesc('id');
         }
         $this->ingredients = Ingredient::all()->pluck('name', 'id')->toArray();
         $this->recipesIngredients = Recipe::all()->map(function($recipe){
