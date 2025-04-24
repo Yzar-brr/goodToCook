ce2ecce38715d1df3be79276f24500ff8266b142
Sofiane <55191416+Yzar-brr@users.noreply.github.com>
2025-04-24 21:37:09 +0200

    ajout fonctionalité végé

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -17,15 +17,38 @@ class Home extends Component
     public $researchRecipe = '';
     public $principalRecipe;    
     public $search = '';
+    public $vegetarian = false;
     
 
     public function render()
     {
+        // Fetch IDs of ingredients with 'vege_option' set to 1
+    $recette_vege = Ingredient::where('vege_option', 1)->get();
+    $recette_vege = $recette_vege->pluck('id')->toArray();
+
+    // Query recipes that have ingredients with the vege_option condition
+    $recette_vege = Recipe::whereHas('ingredients', function($query) use ($recette_vege) {
+        // Specify which 'id' column to refer to (ingredient's id)
+        $query->whereIn('ingredient.id', $recette_vege);
+    })->get();
+
         $this->principalRecipe = Recipe::where('confirmed', 1)->where('image', '!=', null)->orderBy('id', 'desc')->first() ?? null;
         if($this->researchRecipe != ''){
             $this->recipes = Recipe::where('name', 'like', '%'.$this->researchRecipe.'%')->get()->whereIn('confirmed', 1);
+            if($this->vegetarian){
+                $this->recipes = $this->recipes->filter(function($recipe) {
+                    $ingredientIds = $recipe->ingredients->pluck('vege_option')->toArray();
+                    return !in_array(0, $ingredientIds) && in_array(1, $ingredientIds);
+                });
+            }
         }else{
             $this->recipes = Recipe::all()->whereIn('confirmed', 1)->sortByDesc('id');
+            if($this->vegetarian){
+                $this->recipes = $this->recipes->filter(function($recipe) {
+                    $ingredientIds = $recipe->ingredients->pluck('vege_option')->toArray();
+                    return !in_array(0, $ingredientIds) && in_array(1, $ingredientIds);
+                });
+            }
         }
         $this->ingredients = Ingredient::all()->pluck('name', 'id')->toArray();
         $this->recipesIngredients = Recipe::all()->map(function($recipe){
